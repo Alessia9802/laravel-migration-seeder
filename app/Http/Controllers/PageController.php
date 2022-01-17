fa9934205dedeed6ad6a679771be688f99e1be59
Alessia9802 <alessiamisciagna@yahoo.it>
2022-01-17 16:07:03 +0100

    PageController fixed

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends index(){
+class PageController extends Controller
+{
     
-    ddd(Travel::all()); 
+   //
 }
